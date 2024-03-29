07d2320f1600fd8ab7318abc099103c08444a692
zkraken <zcsouleymane@gmail.com>
2024-03-29 11:22:27 +0100

    Shader code

--- main.cpp
+++ main.cpp
@@ -2,6 +2,24 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+
+const char* vertexShaderSource = 
+"#version 330 core\n"
+"layout (location=0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"	gl_Position = vec4(aPos, 1.0f);\n"
+"}\0";
+
+const char* fragmentShaderSource=
+"#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"	FragColor = vec4(1.0f, 1.0f, 1.0f, 1.0f);\n"
+"}\0";
+
+
 int main(void)
 {
 	if(!glfwInit())
