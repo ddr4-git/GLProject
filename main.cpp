b15d8f5b5a82051945f91d2ddbba77ff507c8d1f
ddr4-git <zcsouleymane@gmail.com>
2024-03-29 14:55:52 +0100

    shader program added

--- main.cpp
+++ main.cpp
@@ -19,7 +19,6 @@ const char* fragmentShaderSource=
 "	FragColor = vec4(1.0f, 1.0f, 1.0f, 1.0f);\n"
 "}\0";
 
-
 int main(void)
 {
 	if(!glfwInit())
@@ -41,6 +40,56 @@ int main(void)
 	}
 
 	glfwMakeContextCurrent(window);
+	gladLoadGL();
+
+	unsigned int vertexShader, fragmentShader;
+
+	int success;
+	char infoLog[512];
+
+	//vertex shader
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource,NULL);
+	glCompileShader(vertexShader);
+
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if(!success)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std :: cerr << "vertex shader creation failed" << infoLog << std :: endl;
+		return -1;
+	}
+
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if(!success)
+	{
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		std :: cerr << "fragment shader creation failed" << infoLog << std :: endl;
+		return -1;
+	}
+	//glShaderSource(fragmentShader, 1, fragmentShader)
+	//fragment shader
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+
+	unsigned int shaderProgram;
+	shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if(!success)
+	{
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		std :: cerr << "shader program failed" << std :: endl;
+		return -1;
+	}
 
 	while(!glfwWindowShouldClose(window))
 	{
