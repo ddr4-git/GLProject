545e07f3b8c4012b9374799bdc15b158338d3457
ddr4-git <zcsouleymane@gmail.com>
2024-03-30 09:45:54 +0100

    my triangle

--- main.cpp
+++ main.cpp
@@ -6,17 +6,20 @@
 const char* vertexShaderSource = 
 "#version 330 core\n"
 "layout (location=0) in vec3 aPos;\n"
+"out vec3 aColor;\n"
 "void main()\n"
 "{\n"
 "	gl_Position = vec4(aPos, 1.0f);\n"
+"	aColor = vec3(0.0f, 1.0f, 1.0f);\n"
 "}\0";
 
 const char* fragmentShaderSource=
 "#version 330 core\n"
 "out vec4 FragColor;\n"
+"in vec3 aColor;\n"
 "void main()\n"
 "{\n"
-"	FragColor = vec4(1.0f, 1.0f, 1.0f, 1.0f);\n"
+"	FragColor = vec4(aColor, 1.0f);\n"
 "}\0";
 
 float vertices[] = 
